0397a0cc5c5b2c24be8ae117158babafdef9c612
Ben Bowles <benjamin.d.bowles@gmail.com>
2023-02-24 22:05:54 -0600

    data half done

--- src/app/db.js
+++ src/app/db.js
@@ -1,23 +1,9 @@
 const path = require("path");
 const fs = require("fs");
 
-const DATA_PATH = "../data.json";
-const getData = async () =>
-{
-    const a = await fs.readFile( path.join( __dirname, DATA_PATH ), "utf8", ( err, data ) =>
-    {
-        if ( err )
-        {
-            console.log( err );
-        }
-        else
-        {
-            return data;
-        }
-    });
-
-    console.log(a)
-    return a;
+const DATA_PATH = "./data.json";
+const getData = async () => {
+  return JSON.parse(fs.readFileSync(DATA_PATH, "utf8"));
 };
 
 const db = {
