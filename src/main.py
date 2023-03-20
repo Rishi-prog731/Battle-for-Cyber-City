9ffef0c7118a2f1abe1d64e81aafa496aa2245f6
Ben Bowles <benjamin.d.bowles@gmail.com>
2023-03-20 16:34:07 -0500

    websockets

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
+from flask_sock import Sock
 
 app = Flask(__name__)
+sock = Sock(app)
 
 @app.get('/')
 def default():
     return render_template('default.html.jinja')
+
+@sock.route('/')
+def ws(ws):
+    while True:
+        message = ws.receive()
+        ws.send(message)
