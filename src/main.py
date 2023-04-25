dbaddb91e8e56fa53d421be8733ba260f9960e12
Benjamin Bowles <benjamin.d.bowles@gmail.com>
2023-04-25 13:44:42 -0500

    comment out prints

--- src/main.py
+++ src/main.py
@@ -9,14 +9,14 @@ Y_EW = ModbusSystem(4)
 R_EW = ModbusSystem(5)
 
 # Print Lights
-print(
-    G_NS,
-    Y_NS,
-    R_NS,
-    G_EW,
-    Y_EW,
-    R_EW
-)
+#print(
+#    G_NS,
+#    Y_NS,
+#    R_NS,
+#    G_EW,
+#    Y_EW,
+#    R_EW
+#)
 
 # Traffic Lights
 NS = TrafficLight(R_NS, Y_NS, G_NS)
@@ -25,18 +25,18 @@ EW = TrafficLight(R_EW, Y_EW, G_EW)
 TRAFFICLIGHTS = [NS, EW]
 
 # Print Traffic Lights
-print(
-    NS,
-    EW
-)
+#print(
+#    NS,
+#    EW
+#)
 
 # Power Grid
 POWER_GRID = System()
 
 # Print Power Grid
-print(
-    POWER_GRID
-)
+#print(
+#    POWER_GRID
+#)
 
 # Power Systems
 BD = Power(0, POWER_GRID)
@@ -49,14 +49,14 @@ RE = Power(5, POWER_GRID)
 grid = [BD, HP, PF, IN, UN, RE]
 
 # Print Power Systems
-print(
-    BD,
-    HP,
-    PF,
-    IN,
-    UN,
-    RE
-)
+#print(
+#    BD,
+#    HP,
+#    PF,
+#    IN,
+#    UN,
+#    RE
+#)
 
 # Special Districts
 H_P = ModbusSystem(0)
@@ -69,10 +69,10 @@ PF_G = ModbusSystem(1)
 POLICE_FIRE = SpecialDistrict("Police/Fire", PF, PF_P, PF_G)
 
 # Print Special Districts
-print(
-    HOSPITAL,
-    POLICE_FIRE
-)
+#print(
+#    HOSPITAL,
+#    POLICE_FIRE
+#)
 
 # Actual City Distrcit Objects with Mappings
 BUSINESS = District("Business", BD)
@@ -84,9 +84,9 @@ DISTRICTS = [
     BUSINESS, HOSPITAL, POLICE_FIRE, INDUSTRIAL, UNIVERSITY, RESIDENTIAL]
 
 # Print Districts
-print(
-    BUSINESS,
-    INDUSTRIAL,
-    UNIVERSITY,
-    RESIDENTIAL
-)
+#print(
+#    BUSINESS,
+#    INDUSTRIAL,
+#    UNIVERSITY,
+#    RESIDENTIAL
+#)
