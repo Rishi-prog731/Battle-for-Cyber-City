913cfb29822d309869c609372f13f1a7d8b668f0
Benjamin Bowles <benjamin.d.bowles@gmail.com>
2023-05-01 11:35:34 -0500

    TODO

--- src/cyber_city/game/objects/__init__.py
+++ src/cyber_city/game/objects/__init__.py
@@ -1,3 +1,18 @@
 """
 Object class's used in the game are defined here.
 """
+
+from .ability import *
+from .event import *
+from .role import *
+
+
+def read_objects_from_excel():
+    """
+    !TODO
+        - read from excel file and create objects 
+        - function should take in a file path
+        - function should return a dictionary of lists of objects
+        - !NOTE: make sure you freeze any dependencies into `requirements.txt`
+    """
+    pass
